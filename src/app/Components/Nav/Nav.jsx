956eb61d48fe6137f3c2b76d5aa47577441879ee
RickyAnchores <ricky23anchores@gmail.com>
2024-09-25 10:34:48 +0200

    update contacts, gallery, nav

--- src/app/Components/Nav/Nav.jsx
+++ src/app/Components/Nav/Nav.jsx
@@ -2,9 +2,9 @@ import React from 'react'
 
 const Nav = () => {
   return (
-    <div className='Nav bg-black text-white flex justify-around items-center'>
-        <div className="logo">NAV</div>
-        <ul className="navList">
+    <div className='Nav bg-black text-white p-4  flex justify-around items-center'>
+        <div className="logo font-bold text-2xl">NAV</div>
+        <ul className="navList flex justify-center items-center gap-4">
             <li>Home</li>
             <li>About</li>
             <li>Contact</li>
--- src/app/pages/Gallery/Gallery.jsx
+++ src/app/pages/Gallery/Gallery.jsx
@@ -3,8 +3,12 @@ import React from 'react'
 const Gallery = () => {
   return (
     <div className='Gallery min-h-screen grid justify-center items-center'>
-        <h1>Gallery</h1>
-        <p>This is Gallery</p>
+        <h1 className='font-extrabold text-4xl text-center'>Gallery</h1>
+        <div className="gallery-container  p-4 border-black grid grid-cols-1 md:grid-cols-3 gap-4">
+          <img className='bg-slate-900 p-4 text-white' src="" alt="image one" />
+          <img className='bg-slate-900 p-4 text-white' src="" alt="image two" />
+          <img className='bg-slate-900 p-4 text-white' src="" alt="image three" />
+        </div>
     </div>
   )
 }
